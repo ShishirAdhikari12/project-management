bd40f68238866b065e6a17cc621936f545746204
Shishir Adhikari <shishiradhikari1829@gmail.com>
2026-08-14 12:14:31 +0000

    Add pagination component and project status constants; update project index to utilize new status mapping

--- resources/js/Pages/Project/Index.jsx
+++ resources/js/Pages/Project/Index.jsx
@@ -1,3 +1,5 @@
+import Pagination from "@/Components/Pagination";
+import { PROJECT_STATUS_CLASS_MAP, PROJECT_STATUS_TEXT_MAP } from "@/constants.jsx";
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
 import { Head, Link } from "@inertiajs/react";
 
@@ -32,18 +34,24 @@ export default function Index({ projects }) {
                 </thead>
                 <tbody>
                   {projects.data.map((project) => (
-                    <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                    <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700" key={project.id}>
                       <td className="px-3 py-2">{project.id}</td>
                       <td className="px-3 py-2">
                         <img src={project.image_path} style={{ width: 80 }} alt="project image" />
                       </td>
                       <td className="px-3 py-2">{project.name}</td>
-                      <td className="px-3 py-2">{project.status}</td>
-                      <td className="px-3 py-2">{project.created_at}</td>
-                      <td className="px-3 py-2">{project.due_date}</td>
+                      <td className="px-3 py-2 text-nowrap">
+                        <span className={"px-2 py-1 rounded text-white " +
+                          PROJECT_STATUS_CLASS_MAP[project.status]
+                        }>
+                          {PROJECT_STATUS_TEXT_MAP[project.status]}
+                        </span>
+                      </td>
+                      <td className="px-3 py-2 text-nowrap">{project.created_at}</td>
+                      <td className="px-3 py-2 text-nowrap">{project.due_date}</td>
                       <td className="px-3 py-2">{project.createdBy.name}</td>
                       <td className="px-3 py-2">
-                        
+
 
                         <Link href={route('project.destroy', project.id)}
                           className="font-medium text-blue-600 dark:text-blue-500 hover:underline mx-1">
@@ -59,7 +67,7 @@ export default function Index({ projects }) {
                   ))}
                 </tbody>
               </table>
-
+              <Pagination links={projects.meta.links} />
             </div>
           </div>
         </div>
